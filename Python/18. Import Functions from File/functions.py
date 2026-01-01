884c6e24d8bb0ab77c8082c8072be076a978d852
Rifky Ramdhani <rifkyramdhani@gmail.com>
2026-01-01 18:28:03 +0700

    Learning module and functions

---
+++ Python/18. Import Functions from File/functions.py
@@ -0,0 +1,6 @@
+def cyl_vol(radius,height):
+    volume = 3.14 * (radius**2) * height
+    return round(volume,1)
+
+def cuboid_vol(l, w, h):
+    return l * w * h
---
+++ Python/18. Import Functions from File/main.py
@@ -0,0 +1,4 @@
+import functions as f
+
+print(f.cuboid_vol(7,8,9))
+print(f.cyl_vol(7,10))
